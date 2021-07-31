d3e054ed5487bcdf16a35f27704a1206c9f34335
icoder18 <deekshaamadan@gmail.com>
2021-07-31 20:38:45 +0530

    completed

--- curr_convert/currency/views.py
+++ curr_convert/currency/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from datetime import date
 
 # Create your views here.
 
 def home(request):
+	if request.method == 'POST':
+		from_curr = request.POST.get('from')
+		print(from_curr)
+		to_curr = request.POST.get('to')
+		print(to_curr)
+		amt = float(request.POST.get('amt'))
+		print(amt)
+
+		ans=0
+
+		today = date.today()
+		today = today.strftime("%Y-%m-%d")
+		
+		#base = EUR
+		url = "http://data.fixer.io/api/"+today
+		access_key = '2364f45848fa03cd95188a983ad7e2f5'
+		querystring = {"format":"json","access_key":access_key}
+
+		response = requests.request("GET", url, params=querystring)
+
+		response = response.json()	
+		print(response)
+		rates = response['rates']
+		b1 = float(rates[from_curr])
+		b2 = float(rates[to_curr])
+		print(b1,b2)
+
+		euros = amt/b1
+		euros = euros*b2
+		ans=round(euros,2)
+
+		context={
+			'ans':ans
+		}
+
+		return render(request, 'currency/home.html',context)
+
 	return render(request, 'currency/home.html')
